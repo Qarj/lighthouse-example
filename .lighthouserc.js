82a0b2f165d5e9a21bfa4c3e2ebe16cd33b3fd60
Tim Buckland <tim.buckland@stepstone.com>
2022-03-29 17:13:52 +0100

    fix: try full cookie

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,7 +2,10 @@ module.exports = {
   ci: {
     collect: {
       settings: {
-        extraHeaders: JSON.stringify({ Cookie: "CONSENTMGR=consent:true" }),
+        extraHeaders: JSON.stringify({
+          Cookie:
+            "CONSENTMGR=c1:1%7Cc2:1%7Cc3:1%7Cc4:1%7Cc5:0%7Cc6:1%7Cc7:1%7Cc8:0%7Cc9:1%7Cc10:0%7Cc11:0%7Cc12:1%7Cc13:1%7Cc14:0%7Cc15:0%7Cts:1648382863749%7Cconsent:true",
+        }),
       },
     },
   },
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # lighthouse-example
 
 ![Unauth Mobile](https://github.com/Qarj/lighthouse-example/workflows/Unauth%20Mobile/badge.svg)
+![Unauth Mobile](https://github.com/Qarj/lighthouse-example/workflows/.github/workflows/unauthMobile.yml/badge.svg)
